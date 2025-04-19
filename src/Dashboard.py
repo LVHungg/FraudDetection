cf31a85218d7c5ee79cf860fc74d9f308c9f7905
Your Name <your_email@example.com>
2025-04-19 22:04:18 +0700

    Add dashboard, update EDA, and fraud detection image

---
+++ src/Dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+import os
+import pandas as pd
+
+icon_path = os.path.join("icons", "frauddetection.png")
+
+st.set_page_config(
+    page_title = 'Fraud Detection Monitoring',
+    page_icon = 'frauddetection.png',
+    # initial_sidebar_state="expanded",
+    layout = 'wide',
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# ",
+        'Get help': 'mailto:levanhung2611@gmail.com'
+    }
+)
+
+
+st.header('Fraud Detection Monitoring')
+df = pd.read_csv('C:\\Users\\levan\\FraudDetection\\data\\Fraud Detection Dataset.csv')
+
+st.dataframe(df)
